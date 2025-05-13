0ed47085a441fdaae34cff9ad89c82e3d793d9ed
Pavel555-star <77859008+Pavel555-star@users.noreply.github.com>
2025-05-13 11:39:25 +0200

    Add files via upload

---
+++ Quantum_calculations/basis_set_TPSS_orbital/test_2.sh
@@ -0,0 +1,317 @@
+'./basis_set_TPSS_orbital.out' H 25 0.99 80
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 1 0 0 -0.5
+
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 2 0 0 0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 2 0 0 -0.5
+
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 2 1 1 0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 2 1 1 -0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 2 1 0 0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 2 1 0 -0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 2 1 -1 0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 2 1 -1 -0.5
+
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 3 0 0 0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 3 0 0 -0.5
+
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 3 1 1 0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 3 1 1 -0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 3 1 0 0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 3 1 0 -0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 3 1 -1 0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 3 1 -1 -0.5
+
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 3 2 2 0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 3 2 2 -0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 3 2 1 0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 3 2 1 -0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 3 2 0 0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 3 2 0 -0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 3 2 -1 0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 3 2 -1 -0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 3 2 -2 0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 3 2 -2 -0.5
+
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 4 0 0 0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 4 0 0 -0.5
+
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 4 1 1 0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 4 1 1 -0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 4 1 0 0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 4 1 0 -0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 4 1 -1 0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 4 1 -1 -0.5
+
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 4 2 2 0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 4 2 2 -0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 4 2 1 0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 4 2 1 -0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 4 2 0 0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 4 2 0 -0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 4 2 -1 0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 4 2 -1 -0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 4 2 -2 0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 4 2 -2 -0.5
+
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 4 3 3 0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 4 3 3 -0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 4 3 2 0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 4 3 2 -0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 4 3 1 0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 4 3 1 -0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 4 3 0 0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 4 3 0 -0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 4 3 -1 0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 4 3 -1 -0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 4 3 -2 0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 4 3 -2 -0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 4 3 -3 0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 4 3 -3 -0.5
+
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 5 0 0 0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 5 0 0 -0.5
+
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 5 1 1 0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 5 1 1 -0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 5 1 0 0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 5 1 0 -0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 5 1 -1 0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 5 1 -1 -0.5
+
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 5 2 2 0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 5 2 2 -0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 5 2 1 0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 5 2 1 -0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 5 2 0 0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 5 2 0 -0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 5 2 -1 0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 5 2 -1 -0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 5 2 -2 0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 5 2 -2 -0.5
+
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 5 3 3 0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 5 3 3 -0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 5 3 2 0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 5 3 2 -0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 5 3 1 0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 5 3 1 -0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 5 3 0 0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 5 3 0 -0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 5 3 -1 0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 5 3 -1 -0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 5 3 -2 0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 5 3 -2 -0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 5 3 -3 0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 5 3 -3 -0.5
+
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 5 4 4 0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 5 4 4 -0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 5 4 3 0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 5 4 3 -0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 5 4 2 0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 5 4 2 -0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 5 4 1 0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 5 4 1 -0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 5 4 0 0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 5 4 0 -0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 5 4 -1 0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 5 4 -1 -0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 5 4 -2 0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 5 4 -2 -0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 5 4 -3 0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 5 4 -3 -0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 5 4 -4 0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 5 4 -4 -0.5
+
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 6 0 0 0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 6 0 0 -0.5
+
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 6 1 1 0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 6 1 1 -0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 6 1 0 0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 6 1 0 -0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 6 1 -1 0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 6 1 -1 -0.5
+
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 6 2 2 0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 6 2 2 -0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 6 2 1 0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 6 2 1 -0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 6 2 0 0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 6 2 0 -0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 6 2 -1 0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 6 2 -1 -0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 6 2 -2 0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 6 2 -2 -0.5
+
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 6 3 3 0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 6 3 3 -0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 6 3 2 0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 6 3 2 -0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 6 3 1 0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 6 3 1 -0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 6 3 0 0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 6 3 0 -0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 6 3 -1 0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 6 3 -1 -0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 6 3 -2 0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 6 3 -2 -0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 6 3 -3 0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 6 3 -3 -0.5
+
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 6 4 4 0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 6 4 4 -0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 6 4 3 0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 6 4 3 -0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 6 4 2 0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 6 4 2 -0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 6 4 1 0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 6 4 1 -0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 6 4 0 0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 6 4 0 -0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 6 4 -1 0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 6 4 -1 -0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 6 4 -2 0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 6 4 -2 -0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 6 4 -3 0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 6 4 -3 -0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 6 4 -4 0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 6 4 -4 -0.5
+
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 6 5 5 0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 6 5 5 -0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 6 5 4 0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 6 5 4 -0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 6 5 3 0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 6 5 3 -0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 6 5 2 0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 6 5 2 -0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 6 5 1 0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 6 5 1 -0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 6 5 0 0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 6 5 0 -0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 6 5 -1 0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 6 5 -1 -0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 6 5 -2 0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 6 5 -2 -0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 6 5 -3 0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 6 5 -3 -0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 6 5 -4 0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 6 5 -4 -0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 6 5 -5 0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 6 5 -5 -0.5
+
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 7 0 0 0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 7 0 0 -0.5
+
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 7 1 1 0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 7 1 1 -0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 7 1 0 0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 7 1 0 -0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 7 1 -1 0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 7 1 -1 -0.5
+
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 7 2 2 0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 7 2 2 -0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 7 2 1 0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 7 2 1 -0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 7 2 0 0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 7 2 0 -0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 7 2 -1 0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 7 2 -1 -0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 7 2 -2 0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 7 2 -2 -0.5
+
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 7 3 3 0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 7 3 3 -0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 7 3 2 0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 7 3 2 -0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 7 3 1 0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 7 3 1 -0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 7 3 0 0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 7 3 0 -0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 7 3 -1 0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 7 3 -1 -0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 7 3 -2 0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 7 3 -2 -0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 7 3 -3 0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 7 3 -3 -0.5
+
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 7 4 4 0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 7 4 4 -0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 7 4 3 0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 7 4 3 -0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 7 4 2 0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 7 4 2 -0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 7 4 1 0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 7 4 1 -0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 7 4 0 0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 7 4 0 -0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 7 4 -1 0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 7 4 -1 -0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 7 4 -2 0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 7 4 -2 -0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 7 4 -3 0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 7 4 -3 -0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 7 4 -4 0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 7 4 -4 -0.5
+
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 7 5 5 0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 7 5 5 -0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 7 5 4 0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 7 5 4 -0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 7 5 3 0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 7 5 3 -0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 7 5 2 0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 7 5 2 -0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 7 5 1 0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 7 5 1 -0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 7 5 0 0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 7 5 0 -0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 7 5 -1 0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 7 5 -1 -0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 7 5 -2 0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 7 5 -2 -0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 7 5 -3 0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 7 5 -3 -0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 7 5 -4 0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 7 5 -4 -0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 7 5 -5 0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 7 5 -5 -0.5
+
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 7 6 6 0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 7 6 6 -0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 7 6 5 0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 7 6 5 -0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 7 6 4 0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 7 6 4 -0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 7 6 3 0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 7 6 3 -0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 7 6 2 0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 7 6 2 -0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 7 6 1 0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 7 6 1 -0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 7 6 0 0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 7 6 0 -0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 7 6 -1 0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 7 6 -1 -0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 7 6 -2 0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 7 6 -2 -0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 7 6 -3 0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 7 6 -3 -0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 7 6 -4 0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 7 6 -4 -0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 7 6 -5 0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 7 6 -5 -0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 7 6 -6 0.5
+'./basis_set_TPSS_orbital.out' H 25 0.99 80 0 7 6 -6 -0.5
+
+
+
+
+
+
+
+
+
+
