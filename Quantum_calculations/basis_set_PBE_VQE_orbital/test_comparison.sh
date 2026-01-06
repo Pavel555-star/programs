abdf551eab2ba3cf5ba6c9ea7601de79af3f2bec
Pavel555-star <77859008+Pavel555-star@users.noreply.github.com>
2026-01-07 00:13:11 +0100

    Add files via upload

---
+++ Quantum_calculations/basis_set_PBE_VQE_orbital/test_comparison.sh
@@ -0,0 +1,9 @@
+'./basis_set_PBE_VQE_orbital.out' He 50 0.99999 150
+'./basis_set_PBE_VQE_orbital.out' H_H_[1.40_0.00_0.00]_{1_2_1} 25 0.999 100
+'./basis_set_PBE_VQE_orbital.out' H_H_[0.00_1.40_0.00]_{1_2_1} 25 0.999 100
+'./basis_set_PBE_VQE_orbital.out' H_H_[0.00_0.00_1.40]_{1_2_1} 25 0.999 100
+
+'./basis_set_PBE_VQE_orbital.out' He 50 0.99999 150
+'./basis_set_TPSS_orbital.out' H_H_[1.40_0.00_0.00]_{1_2_1} 25 0.999 100
+'./basis_set_TPSS_orbital.out' H_H_[0.00_1.40_0.00]_{1_2_1} 25 0.999 100
+'./basis_set_TPSS_orbital.out' H_H_[0.00_0.00_1.40]_{1_2_1} 25 0.999 100
